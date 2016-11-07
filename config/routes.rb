ab6589d8656b656c0cd9cd3c4963aad1b1602bba
Sarah <sarahlee517@gmail.com>
2016-11-07 17:18:12 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-   resources :groups do
-   	member do
-   		post :join
-   		post :quit
-   	end
-   	resources :posts do
-   		collection do
+  	resources :groups do
+   		member do
+   			post :join
+   			post :quit
+	   	end
+	   resources :posts do
+	   	collection do
 	   		get :hot
 	   	end
    	end
-   end
+  end
+
+  namespace :account do
+  	resources :groups
+  end
+
    root 'groups#index'
 end
