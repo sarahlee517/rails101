4b2d3ac93324d857e2ad45195bbde198f05df6be
Sarah <sarahlee517@gmail.com>
2016-11-10 14:38:09 +0800

    implement posts edit and delete

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,12 @@ class Account::PostsController < ApplicationController
 	def index
 		@posts = current_user.posts
 	end
+
+
+
+	# private
+
+	# def post_params
+	# 	params.require(:post).permit(:content)
+	# end
 end
